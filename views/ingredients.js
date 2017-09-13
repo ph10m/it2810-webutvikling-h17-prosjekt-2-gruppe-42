fab1d2afe5c6a8c8e1f61683068755e3ebca3fc1
Erlend S <erl1sharpy@gmail.com>
2017-09-13 13:42:25 +0200

    Added instructions

--- views/ingredients.js
+++ views/ingredients.js
@@ -1,59 +1,59 @@
 //https://www.tine.no/oppskrifter/kaker/vafler-og-smakaker/verdens-beste-vafler
 //1 porsjon
 var waffle = [
-    { "ingredient":"Egg", "amount": 2, "unit":"stk" },
-    { "ingredient":"Sukker", "amount": 1, "unit":"dl" },
-    { "ingredient":"Skumma Kulturmjølk", "amount": 2, "unit":"dl" },
-    { "ingredient":"Lettmelk", "amount": 1.5, "unit":"dl" },
-    { "ingredient":"Vann", "amount": 1, "unit":"dl" },
-    { "ingredient":"Hvetemel", "amount": 350, "unit":"g" },
-    { "ingredient":"Bakepulver", "amount": 1, "unit":"ts" },
-    { "ingredient":"Natron", "amount": 0.5, "unit":"stk" },
-    { "ingredient":"Vaniljesukker", "amount": 1, "unit":"ts" },
-    { "ingredient":"Kardemomme", "amount": 0.5, "unit":"ts" },
-    { "ingredient":"Smør", "amount": 125, "unit":"g" }
+    { ingredient:"Egg", amount: 2, unit:"stk" },
+    { ingredient:"Sukker", amount: 1, unit:"dl" },
+    { ingredient:"Skumma Kulturmjølk", amount: 2, unit:"dl" },
+    { ingredient:"Lettmelk", amount: 1.5, unit:"dl" },
+    { ingredient:"Vann", amount: 1, unit:"dl" },
+    { ingredient:"Hvetemel", amount: 350, unit:"g" },
+    { ingredient:"Bakepulver", amount: 1, unit:"ts" },
+    { ingredient:"Natron", amount: 0.5, unit:"stk" },
+    { ingredient:"Vaniljesukker", amount: 1, unit:"ts" },
+    { ingredient:"Kardemomme", amount: 0.5, unit:"ts" },
+    { ingredient:"Smør", amount: 125, unit:"g" }
 ];
 
 //https://www.matprat.no/oppskrifter/gjester/marsipankake/
 //12 porsjoner
 var cake = [
-    { "ingredient":"Egg", "amount": 5, "unit":"stk" },
-    { "ingredient":"Sukker", "amount": 150, "unit":"g" },
-    { "ingredient":"Hvetemel", "amount": 150, "unit":"g" },
-    { "ingredient":"Bakepulver", "amount": 150, "unit":"g" },
-    { "ingredient":"Kremfløte", "amount": 1, "unit":"ts" },
-    { "ingredient":"Melis", "amount": 2, "unit":"ss" },
-    { "ingredient":"Melk/Eplejuice til dypping", "amount": 1, "unit":"dl" },
-    { "ingredient":"Bringebærsyltetøy", "amount": 8, "unit":"ss" },
-    { "ingredient":"Grovhakkede valnøttkjerner", "amount": 50, "unit":"g" },
-    { "ingredient":"Marsipan", "amount": 500, "unit":"g" },
-    { "ingredient":"Kokesjokolade", "amount": 40, "unit":"g" },
-    { "ingredient":"Jordbær", "amount": 1, "unit":"stk" },
-    { "ingredient":"Sitronmelisse", "amount": 6, "unit":"stilker" }
+    { ingredient:"Egg", amount: 5, unit:"stk" },
+    { ingredient:"Sukker", amount: 150, unit:"g" },
+    { ingredient:"Hvetemel", amount: 150, unit:"g" },
+    { ingredient:"Bakepulver", amount: 150, unit:"g" },
+    { ingredient:"Kremfløte", amount: 1, unit:"ts" },
+    { ingredient:"Melis", amount: 2, unit:"ss" },
+    { ingredient:"Melk/Eplejuice til dypping", amount: 1, unit:"dl" },
+    { ingredient:"Bringebærsyltetøy", amount: 8, unit:"ss" },
+    { ingredient:"Grovhakkede valnøttkjerner", amount: 50, unit:"g" },
+    { ingredient:"Marsipan", amount: 500, unit:"g" },
+    { ingredient:"Kokesjokolade", amount: 40, unit:"g" },
+    { ingredient:"Jordbær", amount: 1, unit:"stk" },
+    { ingredient:"Sitronmelisse", amount: 6, unit:"stilker" }
 ];
 
 //http://trinesmatblogg.no/2011/10/05/havrekjeks-med-sjokoladebiter-2/
 //24 kjeks, 12 porsjoner?
 var cookies = [
-    { "ingredient":"Egg", "amount": 2, "unit":"stk" },
-    { "ingredient":"Smør", "amount": 200, "unit":"g" },
-    { "ingredient":"Havregryn", "amount": 100, "unit":"g" },
-    { "ingredient":"Hvitt sukker", "amount": 125, "unit":"g" },
-    { "ingredient":"Brunt sukker", "amount": 125, "unit":"g" },
-    { "ingredient":"Hvetemel", "amount": 350, "unit":"g" },
-    { "ingredient":"Natron", "amount": 1, "unit":"ts" },
-    { "ingredient":"Vaniljeekstrakt", "amount": 1, "unit":"ts" },
-    { "ingredient":"Sjokolade", "amount": 200, "unit":"g" }
+    { ingredient:"Egg", amount: 2, unit:"stk" },
+    { ingredient:"Smør", amount: 200, unit:"g" },
+    { ingredient:"Havregryn", amount: 100, unit:"g" },
+    { ingredient:"Hvitt sukker", amount: 125, unit:"g" },
+    { ingredient:"Brunt sukker", amount: 125, unit:"g" },
+    { ingredient:"Hvetemel", amount: 350, unit:"g" },
+    { ingredient:"Natron", amount: 1, unit:"ts" },
+    { ingredient:"Vaniljeekstrakt", amount: 1, unit:"ts" },
+    { ingredient:"Sjokolade", amount: 200, unit:"g" }
 ];
 
 //https://www.dn.no/smak/2017/04/11/1045/Mat/drops-slik-oldemor-likte-dem
 //1 porsjon
 var candy = [
-    { "ingredient":"Farin", "amount": 450, "unit":"g" },
-    { "ingredient":"Glukose", "amount": 225, "unit":"g" },
-    { "ingredient":"Vann", "amount": 1.5, "unit":"dl" },
-    { "ingredient":"Sitronsyre", "amount": 2, "unit":"ts" },
-    { "ingredient":"Sitronessens/sitronaroma", "amount": 1, "unit":"ml" },
-    { "ingredient":"Gul konditorfarge", "amount": 1, "unit":"ts" }
+    { ingredient:"Farin", amount: 450, unit:"g" },
+    { ingredient:"Glukose", amount: 225, unit:"g" },
+    { ingredient:"Vann", amount: 1.5, unit:"dl" },
+    { ingredient:"Sitronsyre", amount: 2, unit:"ts" },
+    { ingredient:"Sitronessens/sitronaroma", amount: 1, unit:"ml" },
+    { ingredient:"Gul konditorfarge", amount: 1, unit:"ts" }
 ]
 
