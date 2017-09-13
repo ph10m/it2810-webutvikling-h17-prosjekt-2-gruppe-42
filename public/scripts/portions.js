79d876c69814e16798ef23971c08434b6c89273e
Erlend S <erl1sharpy@gmail.com>
2017-09-13 14:55:31 +0200

    Working portion selector

--- public/scripts/portions.js
+++ public/scripts/portions.js
@@ -1,12 +1,32 @@
-		// <button class="portions reduce">-</button>
-		// <input class="portions_view" type="text" value="<%= DefaultPortions %>"></input>
-		// <button class="portions increase">+</button>
-        //
-function updatePortions(){
-	// let portionBox = document.getElementById("portions_view");
-	let boxval = $("#portions_view").val();
-	console.log(boxval);
+// <button class="portions reduce">-</button>
+// <input class="portions_view" type="text" value="<%= DefaultPortions %>"></input>
+// <button class="portions increase">+</button>
+//
+$(document).ready(function() {
+    $("#portions_view").on("change paste keyup", function() {
+        updatePortions();
+    });
+});
+
+function decrease() {
+    let boxval = $("#portions_view").val();
+	if(boxval>1) {
+		boxval -= 1;
+	}
+	$("#portions_view").val(boxval);
+	updatePortions();
+}
+
+function increase() {
+    let boxval = eval($("#portions_view").val());
+	boxval += 1;
+    $("#portions_view").val(boxval);
+    updatePortions();
 }
 
 
-// $('input.sitebg').val('000000');
+
+function updatePortions(){
+	let boxval = $("#portions_view").val();
+	console.log(boxval);
+}
